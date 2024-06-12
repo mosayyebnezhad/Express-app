0006ad9ea929dc3e565e2217f373d57c9cf9b202
mosayyebnezhad <mohammadmosayyebnezhad@gmail.com>
2024-06-12 22:09:10 +0330

    Edit 001
    001

--- index.ts
+++ index.ts
@@ -1,8 +1,14 @@
 import * as express from "express";
 import * as path from "path";
+require('dotenv').config()
 
 const app = express();
 const port = parseInt(process.env.PORT) || process.argv[3] || 8080;
+
+
+
+console.log(process.env.PORT);
+
 
 app.use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
