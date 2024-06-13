b9eaa1cda451a4c2500c91463893524131e39f50
mosayyebnezhad <mohammadmosayyebnezhad@gmail.com>
2024-06-13 10:24:49 +0330

    add new option
    send detail from form

--- index.ts
+++ index.ts
@@ -3,6 +3,7 @@ import * as path from "path";
 require('dotenv').config()
 
 const app = express();
+app.use(express.urlencoded({ extended: true }));
 const port = parseInt(process.env.PORT) || process.argv[3] || 8080;
 
 
@@ -15,11 +16,18 @@ app.use(express.static(path.join(__dirname, 'public')))
   .set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-  res.render('index');
+  res.render('index', { name: "محمد" });
+});
+
+
+app.post('/submit', (req, res) => {
+  let { name, email } = req.body;
+
+  res.render('result', { name, email });
 });
 
 app.get('/api', (req, res) => {
-  res.json({"msg": "Hello world"});
+  res.json({ "msg": "Hello world" });
 });
 
 app.listen(port, () => {
